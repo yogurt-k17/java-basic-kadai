fc71b0c4375d97666afc6b7494f5e89ab0fefe9a
yogurt-r17 <ryota.k17@gmail.com>
2025-04-15 23:21:59 +0900

    Add files via upload

--- kadai_028/JaynkenExec_28.java
+++ kadai_028/JaynkenExec_28.java
@@ -4,12 +4,13 @@ public class JaynkenExec_28 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Jyanken_Chapter28 choices = new Jyanken_Chapter28();
+		Jyanken_Chapter28 jyanken = new Jyanken_Chapter28();
 		System.out.println("自分のじゃんけんの手を入力しましょう");
 		System.out.println("グーはrockのrを入力しましょう");
 		System.out.println("チョキはscissorsのsを入力しましょう");
 		System.out.println("パーはpaperのpを入力しましょう");
 
+		jyanken.playGame();
 	}
 
 }
--- kadai_028/Jyanken_Chapter28.java
+++ kadai_028/Jyanken_Chapter28.java
@@ -1,54 +1,81 @@
 package kadai_028;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Jyanken_Chapter28 {
-	//じゃんけんの手を配列に格納
-	HashMap<String, String> choices = new HashMap<>();
+	//じゃんけんの手を配列（map）に格納
+	HashMap<String, String> hands = new HashMap<>();
 
 	//コンストラクタで初期化
 	public Jyanken_Chapter28() {
-		choices.put("p", "パー");
-		choices.put("r", "グー");
-		choices.put("s", "チョキ");
+		hands.put("p", "パー");
+		hands.put("r", "グー");
+		hands.put("s", "チョキ");
 	}
 
 	//メソッド（自分のじゃんけんの手を入力する）
-	public String getMyChoice(String choice) {
-		
-		
+	public String getMyChoice() {
+
+		//scannerオブジェクトの生成
+		Scanner scanner = new Scanner(System.in);
+		//入力した内容を取得
+		String input = scanner.next();
+		//		scanner.close();
+
+		//入力した手が配列にあるか判定
+		//拡張for文で要素を全て取り出す
+		for (Map.Entry<String, String> entry : hands.entrySet()) {
+			//配列のキーと入力したキーが等しくないかの判定
+			if (!entry.getKey().equals(input)) {
+
+			} else {
+				//等しければ値を取得して返す
+				input = entry.getValue();
+				return input;
+			}
+		}
+		//等しくない場合、入力に戻る
+		System.out.println("入力エラー");
+		return getMyChoice();
+
 	}
 
-	//入力した手が配列にあるか判定
-	//for文を使って要素を順番に取り出す
-	for(
+	//メソッド（対戦相手のじゃんけんの手を乱数で選ぶ）
+	public String getRandom() {
+		//じゃんけんの手を配列に格納
+		String[] opponentChoice = { "p", "r", "s" };
+		//インデックスを乱数で生成
+		int index = (int) (Math.random() * 3); //(int)longからintのキャスト
+		//拡張for文で要素を全て取り出す
+		for (Map.Entry<String, String> entry : hands.entrySet()) {
+			//配列のキーと生成したインデックスが等しいか判定
+			if (entry.getKey().equals(opponentChoice[index])) {
+				//等しければ値を取得	
+				opponentChoice[index] = entry.getValue();
+			}
+		}
+		return opponentChoice[index];
 
-	int i = 0;i<choices.size();i++)
-	{
+	}
 
-		//入力した手が配列にある場合
-		if (choices.get(i) == choice) {
-			System.out.println(choices.get(i));
+	//メソッド（じゃんけんを行う）
+	public void playGame() {
+		String myChoice = getMyChoice();
+		String opponent = getRandom();
 
-			//正しいじゃんけんの手でない場合
+		System.out.println("自分の手は" + myChoice + ",対戦相手の手は" + opponent);
+
+		if (myChoice.equals(opponent)) {
+			System.out.println("あいこです");
+		} else if ((myChoice.equals("グー") && opponent.equals("チョキ")) ||
+				(myChoice.equals("チョキ") && opponent.equals("パー")) ||
+				(myChoice.equals("パー") && opponent.equals("グー"))) {
+			System.out.println("自分の勝ちです");
 		} else {
-			System.out.println("入力エラー");
-		}
-	}return(choice);
-	}
-
-	//対戦相手のじゃんけんの手を乱数で選ぶ
-	public String getRandom(long num) {
-		 num = Math.round(Math.random() * 3);
-		//for文を使って要素を順番に取り出す
-		for (int i = 0; i < choices.size(); i++) {
-			//乱数と一致した要素を取り出す
-			if (choices.get(i) == num) {
-				System.out.println(choices.get(i));
+			System.out.println("自分の負けです");
 		}
 
-		return (num);
 	}
-
 }
